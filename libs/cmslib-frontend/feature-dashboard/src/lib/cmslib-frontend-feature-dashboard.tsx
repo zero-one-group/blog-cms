d5763e8a8d2cdd2b7c851b3a84faf27e655fcc50
gelembungsahabat <wildanrgssport@gmail.com>
2021-12-28 18:53:55 +0700

    add usefieldarray on dashboard form

--- libs/cmslib-frontend/feature-dashboard/src/lib/cmslib-frontend-feature-dashboard.tsx
+++ libs/cmslib-frontend/feature-dashboard/src/lib/cmslib-frontend-feature-dashboard.tsx
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 import { TextAreaField, Navbar } from '@cms-blog/cmslib-frontend/ui';
-import { Route } from 'react-router-dom';
-import { Stack, useColorModeValue, Box } from '@chakra-ui/react';
+import { Route, useHistory } from 'react-router-dom';
+import { Stack, useColorModeValue, Box, Button } from '@chakra-ui/react';
+import { useForm, useFieldArray } from 'react-hook-form';
+import { AddIcon } from '@chakra-ui/icons';
 
 /* eslint-disable-next-line */
 export interface CmslibFrontendFeatureDashboardProps {}
@@ -9,16 +11,75 @@ export interface CmslibFrontendFeatureDashboardProps {}
 export function CmslibFrontendFeatureDashboard(
   props: CmslibFrontendFeatureDashboardProps
 ) {
+  const { register, control, handleSubmit } = useForm({
+    defaultValues: {
+      payload: [
+        {
+          modal: null,
+        },
+      ],
+    },
+  });
+  const history = useHistory();
+
+  const { fields, append, remove } = useFieldArray({
+    name: 'payload',
+    control,
+  });
+
+  const onSubmit = async (formData: unknown) => {
+    console.log(formData);
+  };
   return (
     <Stack bg={useColorModeValue('white', 'inherit')} spacing={0}>
-      <Navbar/>
-      <Box m="9" p="9" pt='20' borderColor="gray.300" borderWidth="1px" rounded="lg">
-        <h1>Welcome to CMS-Blog!</h1>
-        <Box mt='5' p="9" pt='6' borderColor="gray.300" borderWidth="1px" rounded="lg">
+      <Navbar />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Box
+          m="9"
+          mt="16"
+          p="9"
+          borderColor="gray.300"
+          borderWidth="1px"
+          rounded="lg"
+        >
+          <h1>Welcome to CMS-Blog!</h1>
+          <Box
+            mt="5"
+            p="9"
+            pt="6"
+            borderColor="gray.300"
+            borderWidth="1px"
+            rounded="lg"
+          >
+            {fields.map((field, index) => {
+              return (
+                <>
+                  <TextAreaField
+                    label="Modal"
+                    {...register(`payload.${index}.modal`, {
+                      required: 'fill the textarea!',
+                    })}
+                  />
 
-        <TextAreaField label="haha" />
+                  {fields.length - 1 === index && (
+                    <Button
+                      w="10%"
+                      onClick={() => append({})}
+                      mt={6}
+                      color="teal.300"
+                      _hover={{ bg: 'tealPrimary.hover' }}
+                      bg="tealPrimary.normal"
+                    >
+                      <AddIcon mr={2} />
+                      Add
+                    </Button>
+                  )}
+                </>
+              );
+            })}
+          </Box>
         </Box>
-      </Box>
+      </form>
     </Stack>
   );
 }
