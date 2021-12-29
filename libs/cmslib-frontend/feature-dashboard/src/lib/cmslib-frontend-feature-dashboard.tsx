1bef8897bdc4900f68b988a1179f7f9548f5ba36
gelembungsahabat <wildanrgssport@gmail.com>
2021-12-29 17:27:29 +0700

    add footer

--- libs/cmslib-frontend/feature-dashboard/src/lib/cmslib-frontend-feature-dashboard.tsx
+++ libs/cmslib-frontend/feature-dashboard/src/lib/cmslib-frontend-feature-dashboard.tsx
@@ -9,6 +9,7 @@ import {
   Button,
   Heading,
   CloseButton,
+  Text,
 } from '@chakra-ui/react';
 import { useForm, useFieldArray } from 'react-hook-form';
 import { AddIcon } from '@chakra-ui/icons';
@@ -19,20 +20,32 @@ export interface CmslibFrontendFeatureDashboardProps {}
 export function CmslibFrontendFeatureDashboard(
   props: CmslibFrontendFeatureDashboardProps
 ) {
+  const year = new Date().getFullYear();
   return (
-    <Stack
-      bg={useColorModeValue('gray.300', 'inherit')}
-      spacing={0}
-      minHeight="100vh"
-    >
-      <Navbar />
-      <Route path="/home" exact render={() => <Home />} />
-      <Route
-        path="/content-management-form"
-        exact
-        render={() => <ContentManagementForm />}
-      />
-    </Stack>
+    <>
+      <Stack
+        bg={useColorModeValue('gray.300', 'inherit')}
+        spacing={0}
+        minHeight="100vh"
+      >
+        <Navbar />
+        <Route path="/home" exact render={() => <Home />} />
+        <Route
+          path="/content-management-form"
+          exact
+          render={() => <ContentManagementForm />}
+        />
+      </Stack>
+      <Stack
+        bg={useColorModeValue('gray.100', 'inherit')}
+        spacing={0}
+        minHeight="10vh"
+      >
+        <Text fontSize="md" mt="5" textAlign="center">
+          Blog-CMS <br /> © {year} Muhammad Wildan Nur Karim
+        </Text>
+      </Stack>
+    </>
   );
 }
 
