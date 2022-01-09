fc0572e3413bc544558da330c5be83921a2d6525
gelembungsahabat <wildanrgssport@gmail.com>
2022-01-09 13:47:25 +0700

    add carousel and content model

---
+++ libs/cmslib-backend/model/src/lib/carousel.model.ts
@@ -0,0 +1,32 @@
+import { Model } from 'objection';
+
+import { TableName } from '@cms-blog//cmslib-backend/interface';
+import { ProjectModel } from './project.model';
+import { BaseModel } from './base.model';
+
+export class CarouselModel extends BaseModel {
+  header!: string;
+  subheader!: string;
+  image_url!: string;
+  project_id!: number;
+  created_at!: Date;
+  updated_at!: Date;
+
+  static tableName = TableName.CAROUSELS;
+}
+
+export class GraphFetchedCarouselModel extends CarouselModel {
+  carousel!: CarouselModel;
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  static relationMappings = (): any => ({
+    project: {
+      modelClass: ProjectModel,
+      relation: Model.BelongsToOneRelation,
+      join: {
+        from: `${TableName.CAROUSELS}.project_id`,
+        to: `${TableName.PROJECTS}.id`,
+      },
+    },
+  });
+}
